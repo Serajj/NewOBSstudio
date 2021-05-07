dd67ce7de4883ca01a620295d42c405b92898bcb
Seraj Alam <rajalam2372@gmail.com>
2021-05-07 19:16:39 +0530

    jhgf

--- app.js
+++ app.js
@@ -154,21 +154,21 @@ nms.on('doneConnect', (id, args) => {
 });
 
 nms.on('prePublish', (id, StreamPath, args) => {
-    // console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
     // // let session = nms.getSession(id);
     // // session.reject();
 });
 
 nms.on('postPublish', (id, StreamPath, args) => {
-    // console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
 
 nms.on('donePublish', (id, StreamPath, args) => {
-    // console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
 
 nms.on('prePlay', (id, StreamPath, args) => {
-    // console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
     // let session = nms.getSession(id);
     // session.reject();
 });
@@ -182,7 +182,7 @@ nms.on('postPlay', (id, StreamPath, args) => {
 });
 
 nms.on('donePlay', (id, StreamPath, args) => {
-    // console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
 
 
