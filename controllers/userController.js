335edf65563d0bea36bd45d0d97f3dc1913edc30
Seraj Alam <rajalam2372@gmail.com>
2021-05-09 02:13:20 +0530

    social auth added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { TOKEN_SECRET_KEY } = require("../config");
 const saveStreamModel = require("../model/saveStreamModel");
+const streamModel = require("../model/streamModel");
+var shortid = require('shortid');
+const socialLoginModel = require("../model/socialLoginModel");
 
 
 
@@ -31,6 +34,54 @@ exports.register = async (req, res, next) => {
         const token = getSignInToken(newUser);
 
         return res.status(200).json({ success: true, message: "Signup and logged In Successfully !!", token: token, data: getUserData(newUser) })
+    } catch (error) {
+        error.status = 400;
+        next(error);
+    }
+}
+
+
+exports.socialUserInfo = async (req, res, next) => {
+    const body = req.body;
+
+    if (!body.name) {
+        res.status(422).json({ success: false, message: "Please Provide  name" });
+    }
+    if (!body.email) {
+        res.status(422).json({ success: false, message: "Please Provide  email" });
+    }
+    if (!body.token) {
+        res.status(422).json({ success: false, message: "Please Provide social auth token" });
+    }
+    if (!body.type) {
+        res.status(422).json({ success: false, message: "Please Provide user type" });
+    }
+
+
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash("mynewpassword", salt)
+    body.password = passwordHash;
+    const { name, type, email, token, password } = body;
+    const phone = "9140327455";
+    console.log(req.body)
+
+    const checkUser = await socialLoginModel.findOne({ email });
+
+    if (checkUser) {
+        return res.status(200).json({ success: true, message: "logged In Successfully !!", token: token, data: getUserData(checkUser) })
+    }
+
+    const newUser = new socialLoginModel({
+        name, phone, type, email, token, password
+    })
+
+    try {
+        await newUser.save();
+
+        const token = getSignInToken(newUser);
+
+        return res.status(200).json({ success: true, message: "logged In Successfully !!", token: token, data: getUserData(newUser) })
     } catch (error) {
         error.status = 400;
         next(error);
@@ -76,7 +127,47 @@ exports.getStream = async (req, res) => {
 }
 
 
+//streams
 
+exports.startStream = async (req, res) => {
+
+    if (!req.body.stream_name) {
+        res.status(422).json({ success: false, message: "Please Provide stream name" });
+    }
+
+    if (!req.body.venue) {
+        res.status(422).json({ success: false, message: "Please Provide stream venue" });
+    }
+
+    if (!req.body.cover_image) {
+        res.status(422).json({ success: false, message: "Please Provide stream cover_image" });
+    }
+    var streamId = shortid.generate();
+    console.log(streamId);
+
+    var myStream = new streamModel({
+        stream_id: streamId,
+        user_id: req.user.id,
+        stream_name: req.body.stream_name,
+        venue: req.body.venue,
+        cover_image: req.body.cover_image
+    })
+
+    try {
+        await myStream.save();
+    } catch (error) {
+        res.status(200).json({ success: false, message: "Unable to start stream", error: error.message });
+    }
+
+
+    // while (!checkUniqueStreamId(streamId)) {
+    //     streamId = shortid.generate();
+    // }
+    console.log("final " + streamId);
+
+
+    res.status(200).json({ success: true, message: "Stream records saved successfully", stream_id: streamId });
+}
 
 getSignInToken = user => {
     return jwt.sign({
@@ -95,3 +186,14 @@ getUserData = user => {
         email: user.email
     })
 }
+
+checkUniqueStreamId = streamId => {
+    streamModel.findOne({ stream_id: streamId }, function (err, obj) {
+
+        if (obj) {
+            return obj;
+        }
+        return "not unique";
+    });
+}
+
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { login, register, getStream } = require("../controllers/userController");
+const { login, register, getStream, startStream, socialUserInfo } = require("../controllers/userController");
 const authToken = require("../middleware/authToken");
 
 const router = express.Router();
@@ -10,7 +10,11 @@ router.get('/', (req, res) => res.send('Hello this is api route!'))
 
 router.post('/login', login)
 
+router.post('/socialLogin', socialUserInfo)
+
 router.post('/register', register)
 
 router.post('/getStreams', authToken, getStream)
+
+router.post('/startStream', authToken, startStream)
 module.exports = router
